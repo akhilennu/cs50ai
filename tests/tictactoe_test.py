98a65ae676aa1225dcdc431ee6edb3c878d3c974
Akhil Ennu <akhil.e2012@gmail.com>
2021-08-15 12:58:18 +0530

    minimax working algorithm

--- tests/tictactoe_test.py
+++ tests/tictactoe_test.py
@@ -1,3 +1,4 @@
+from logging import BASIC_FORMAT
 import tictactoe as t
 import test_helper as helper
 import unittest
@@ -8,16 +9,19 @@ class TestTicTacToe(unittest.TestCase):
         for test_case in helper.test_cases:
             self.assertEqual(t.player(test_case["board"]),test_case["player"])
 
-    def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+    def test_terminal(self):
+        self.assertTrue(t.terminal([['O', 'X', 'O'], ['X', 'X', 'O'], ['X', 'O', 'X']]))
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_result(self):
+        board = [['O', 'O', 'X'], ['X', 'O', 'X'], [t.EMPTY, 'X', t.EMPTY]]
+        tmp = t.result(board,(2,2))
+        self.assertEqual(tmp,[['O', 'O', 'X'], ['X', 'O', 'X'], [t.EMPTY, 'X', 'O']])
+        self.assertEqual(board,[['O', 'O', 'X'], ['X', 'O', 'X'], [t.EMPTY, 'X', t.EMPTY]])
+
+    def test_minimax(self):
+        self.assertEqual(t.minimax([['O', 'X', 'O'], ['X', 'X', 'O'], ['X', 'O', t.EMPTY]]),(2,2))
+        self.assertEqual(t.minimax([['O', 'X', 'O'], ['X', 'X', 'O'], ['X', t.EMPTY, t.EMPTY]]),(2,1))
+        self.assertEqual(t.minimax([['O', 'O', 'X'], ['X', 'O', 'X'], [t.EMPTY, 'X', t.EMPTY]]),(2,2))
 
 if __name__ == '__main__':
     unittest.main()
--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -52,8 +53,11 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    board_copy = board[:]
+    board_copy = copy.deepcopy(board)
     i,j = action
+    if(board_copy[i][j]!=EMPTY):
+        print("invalid action",board,action)
+        raise Exception
     board_copy[i][j] = player(board)
     return board_copy
 
@@ -62,25 +66,75 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if(board[i][0]==board[i][1]==board[i][2] and board[i][0]!=EMPTY):
+            return board[i][0]
+        if(board[0][i]==board[1][i]==board[2][i] and board[0][i]!=EMPTY):
+            return board[0][i]
+    if(((board[0][0]==board[1][1]==board[2][2]) or (board[0][2]==board[1][1]==board[2][0])) and board[1][1]!=EMPTY):
+        return board[1][1]
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if(winner(board)):
+        return True
+    for i in range(3):
+        for j in range(3):
+            if(board[i][j]==EMPTY):
+                return False
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    tmp = winner(board)
+    return 1 if(tmp==X) else -1 if(tmp==O) else 0
+    
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    #Adding alpha beta pruning is pending
+    if(player(board)==X):
+        v,action =  max_value(board)
+        return action
+    else:
+        v,action =  min_value(board)
+        return action
+
+def max_value(board):
+    if(terminal(board)):
+        return utility(board), None
+    v= -2
+    selected_action = None
+    for a in actions(board):
+        tmp, next_action = min_value(result(board,a))
+        if(tmp>v):
+            v= tmp
+            selected_action = a
+        if(v==1):
+            return v, selected_action
+    return v, selected_action
+
+def min_value(board):
+    if(terminal(board)):
+        return utility(board), None
+    v= 2
+    selected_action = None
+    for a in actions(board):
+        tmp, next_action = max_value(result(board,a))
+        if(tmp<v):
+            v= tmp
+            selected_action = a
+        if(v==-1):
+            return v, selected_action
+    return v, selected_action
+
